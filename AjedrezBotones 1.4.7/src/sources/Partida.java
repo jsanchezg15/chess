26ad5041325d578276f3228da4c171b7f0043e02
jsanchezg15 <43110966+jsanchezg15@users.noreply.github.com>
2020-10-31 02:14:43 +0100

    Add files via upload

---
+++ AjedrezBotones 1.4.7/src/sources/Partida.java
@@ -0,0 +1,26 @@
+package sources;
+
+import java.util.Scanner;
+
+public class Partida {
+
+	public static void main(String[] args) {
+		
+		char table[][] = {  {'k', '_', '_', '_', '_', '_', '_', '_'},
+							{'_', '_', '_', '_', '_', 'o', '_', 'o'},
+							{'o', 'O', '_', '_', '_', '_', '_', '_'},
+							{'_', 'A', '_', 'c', '_', '_', '_', '_'},
+							{'_', '_', '_', '_', '_', '_', '_', '_'},
+							{'_', '_', '_', '_', 'O', '_', '_', '_'},
+							{'_', '_', '_', '_', '_', '_', '_', '_'},
+							{'_', '_', '_', '_', 'K', '_', '_', '_'}};
+
+		
+		State st = new State(table, true);
+		
+		//Tablero tablero = new Tablero(st, false, true);		
+		Tablero tablero = new Tablero(true, true);		
+		
+	}
+
+}
